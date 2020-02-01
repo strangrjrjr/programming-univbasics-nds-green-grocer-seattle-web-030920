c69e2c793273de327f6f707cb58d68da7e5b20ab
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 18:07:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,7 +51,9 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
       else
-        
+        cart_item_with_coupon = {
+          
+        }
         
     counter += 1
   end
