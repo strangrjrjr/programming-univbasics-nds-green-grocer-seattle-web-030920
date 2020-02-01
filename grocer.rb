3ef74f5b2c5c9bd0cc6ee2e704c349ac07de2d0c
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 18:24:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -73,7 +73,7 @@ def apply_clearance(cart)
   counter = 0
   while counter < cart.length
     if cart[counter][:clearance]
-      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.2))
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.2)).round(2)
     end
   end
 end
