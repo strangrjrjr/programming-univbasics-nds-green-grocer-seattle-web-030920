23d789cdd3ccdca2bf243ca9b8360ff0ea9e924f
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 18:09:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_coupons(cart, coupons)
           :count => coupons[counter][:num],
           :clearance => cart_item[:clearance]
         }
-        
+        cart << cart_item_with_coupon
     counter += 1
   end
   
