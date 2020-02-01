960c38935246854ab0719e96c5c7cb8aa70ce47b
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 18:22:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,6 +70,9 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < cart.length
+  
 end
 
 def checkout(cart, coupons)
